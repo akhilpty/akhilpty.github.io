2aaa124ff86ec59cefca5647fb02e31f912e65ea
akhilpty <akhilpattazhy1998@gmail.com>
2026-04-03 12:29:11 +0530

    feat: implement contact page with email copy functionality and resume download animation

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { animate } from 'animejs';
 
 @Component({
   selector: 'app-contact',
@@ -10,13 +11,28 @@ import { Component } from '@angular/core';
 export class Contact {
   emailCopied = false;
 
+  @ViewChild('resumeBtn') resumeBtn!: ElementRef;
+
   copyEmail() {
     const email = 'akhilpattazhy1998@gmail.com';
     navigator.clipboard.writeText(email).then(() => {
       this.emailCopied = true;
-      setTimeout(() => {
-        this.emailCopied = false;
-      }, 2000);
+      setTimeout(() => (this.emailCopied = false), 2000);
+    });
+  }
+
+  onDownloadClick() {
+    setTimeout(() => {
+      this.playDownloadAnimation();
+    }, 300);
+  }
+
+  playDownloadAnimation() {
+    animate(this.resumeBtn.nativeElement, {
+      y: [-25, 0], // move up then back down
+      scale: [1.15, 1], // slightly enlarge then back
+      duration: 500,
+      easing: 'easeOutElastic(1,0.6)',
     });
   }
 }
